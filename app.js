8378ed3acc99524ed57c6a5c1759dd5462bad3b7
omegga <substitute.user.do@gmail.com>
2016-07-18 13:34:20 +0200

    fix syntax and replace text/json with application/json

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var http = require("http")
-var server = http.createServer(function(req, res){
-    var h = req.headers
-    var userAgent = h["user-agent"].split("(")[1].split(")")[0]
+var http = require("http");
+var server = http.createServer(function(req, res) {
+    var h = req.headers;
+    var userAgent = h["user-agent"].split("(")[1].split(")")[0];
     var result = {
         ipaddress: h["x-forwarded-for"],
         language :h["accept-language"].slice(0,5),
         software: userAgent
     }
     
-    res.writeHead(200, {"Content-Type":"text/json"});
-    res.end(JSON.stringify(result))
+    res.writeHead(200, {"Content-Type":"application/json"});
+    res.end(JSON.stringify(result));
 })
 
-server.listen(process.env.PORT)
+server.listen(process.env.PORT);
