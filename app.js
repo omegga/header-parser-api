2cca2928e5d69940eee20ca3ca0c91d50040d7bc
omegga <substitute.user.do@gmail.com>
2016-08-03 09:46:42 +0200

    add default port 1337 for server to listen

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var server = http.createServer(function(req, res) {
     var userAgent = h["user-agent"].split("(")[1].split(")")[0];
     var result = {
         ipaddress: h["x-forwarded-for"],
-        language :h["accept-language"].slice(0,5),
+        language: h["accept-language"].slice(0,5),
         software: userAgent
     }
     
     res.writeHead(200, {"Content-Type":"application/json"});
     res.end(JSON.stringify(result));
-})
 
-server.listen(process.env.PORT);
+}).listen(process.env.PORT || 1337);
